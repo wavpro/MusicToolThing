e073d0f0dff0fbd0eac18ac2511d5273601bd460
Wavpro <albin.hedwall@gmail.com>
2024-05-29 22:19:16 +0200

    Added sizing and updated validation

--- src/processing/cover.ts
+++ src/processing/cover.ts
@@ -6,10 +6,13 @@ export default function processCover(id: number): void {
     if (!fs.existsSync(`./data/tracks/${id}`)) {
         return log(`Failed to process cover for track #${id}, no directory to pull data from`);
     }
-    
+
     let files = fs.readdirSync(`./data/tracks/${id}`);
     if (!files.includes("cover.jpg") && !files.includes("cover.png")) {
-        fs.copyFileSync(`./data/default_cover.png`, `./data/tracks/${id}/cover.png`)
+        fs.copyFileSync(`./data/pre_made_img/default_cover.png`, `./data/tracks/${id}/cover.png`)
+        fs.copyFileSync(`./data/pre_made_img/default_cover_512.png`, `./data/tracks/${id}/cover_512.png`)
+        fs.copyFileSync(`./data/pre_made_img/default_cover_128.png`, `./data/tracks/${id}/cover_128.png`)
+        fs.copyFileSync(`./data/pre_made_img/default_cover_64.png`, `./data/tracks/${id}/cover_64.png`)
         return;
     }
 
@@ -18,14 +21,40 @@ export default function processCover(id: number): void {
             .resize(1000, 1000, {
                 fit: sharp.fit.fill,
             })
-            .toFile(`./data/tracks/${id}/cover.png`);
+            .toFile(`./data/tracks/${id}/cover.png`)
+            .then(() => {
+                fs.rmSync(`./data/tracks/${id}/cover.jpg`);
+
+                createSubset(id)
+            })
 
         return;
     }
 
-    sharp(`./data/tracks${id}/cover.png`)
+    sharp(`./data/tracks/${id}/cover.png`)
         .resize(1000, 1000, {
             fit: sharp.fit.fill,
         })
-        .toFile(`./data/tracks/${id}/cover.png`);
+        .toFile(`./data/tracks/${id}/cover.png`)
+        .then(() => {
+            createSubset(id)
+        })
+}
+
+function createSubset(id: number) {
+    sharp(`./data/tracks/${id}/cover.png`)
+        .resize(512, 512, {
+            fit: sharp.fit.cover,
+        })
+        .toFile(`./data/tracks/${id}/cover_512.png`);
+    sharp(`./data/tracks/${id}/cover.png`)
+        .resize(128, 128, {
+            fit: sharp.fit.cover,
+        })
+        .toFile(`./data/tracks/${id}/cover_128.png`);
+    sharp(`./data/tracks/${id}/cover.png`)
+        .resize(64, 64, {
+            fit: sharp.fit.cover,
+        })
+        .toFile(`./data/tracks/${id}/cover_64.png`);
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,17 @@
-import { Cookie, Elysia, t } from 'elysia'
+import { Elysia, t } from 'elysia'
 import type { Database } from "bun:sqlite";
-import format from './processing/formats';
-import initAuth, { type authCookie, isLoggedIn } from './accounts/auth';
+import initAuth, { isLoggedIn } from './accounts/auth';
 import jwt from '@elysiajs/jwt';
 import saveTrack from './processing/save_track';
 import { error } from './response/error';
-import type { user } from '../data/database.sqlite.ts';
+import type { track, user } from '../data/database.sqlite.ts';
 import { response } from './response/response.ts';
+
+const displayTracksPerLoad = 50;
 
 export default function router(db: Database) {
     const getTrackQuery = db.query("SELECT * FROM tracks WHERE id = $id;");
+    const getTracksByUploaderQuery = db.query(`SELECT * FROM tracks WHERE uploaded_by = $uploader_id LIMIT ${displayTracksPerLoad} OFFSET $offset`)
 
     new Elysia()
         .use(
@@ -23,49 +25,98 @@ export default function router(db: Database) {
                 //@ts-ignore
                 beforeHandle: isLoggedIn
             }, (app) => app
-                .get('/tracks/:id/audio', ({ params: { id } }) => {
-                    if (!/^\d+$/.test(id)) {
-                        return error("validation", "params", "id", "Invalid track id");
-                    }
+                .guard(
+                    {
+                        params: t.Object({
+                            id: t.Numeric()
+                        })
+                    },
+                    (app) => app
+                        .get('/tracks/:id/audio', ({ params: { id } }) => {
+                            return Bun.file(`data/tracks/${id}/audio.ogg`);
+                        })
+                        .get('/tracks/:id/info', async ({ params: { id } }) => {
+                            const track = await getTrackQuery.get({ $id: id });
 
-                    return Bun.file(`data/tracks/${id}/audio.ogg`);
-                })
-                .get('/tracks/:id/info', async ({ params: { id } }) => {
-                    if (!/^\d+$/.test(id)) {
-                        return error("validation", "params", "id", "Invalid track id");
-                    }
+                            if (!track) {
+                                return error("validation", "params", "id", "Track doesn't exist");
+                            }
 
-                    const track = await getTrackQuery.get({ $id: id });
+                            return response("", track);
+                        })
+                )
+                .guard(
+                    {
+                        params: t.Object({
+                            id: t.Numeric(),
+                        }),
+                        query: t.Object({
+                            size: t.Union([
+                                t.Literal("1000"),
+                                t.Literal("512"),
+                                t.Literal("128"),
+                                t.Literal("64")
+                            ])
+                        })
+                    },
+                    (app) => app
+                        .get('/tracks/:id/cover', async ({ params: { id }, query: { size } }) => {
+                            if (size === "1000") {
+                                return Bun.file(`data/tracks/${id}/cover.png`);
+                            }
 
-                    if (!track) {
-                        return error("validation", "params", "id", "Track doesn't exist");
-                    }
+                            return Bun.file(`data/tracks/${id}/cover_${size}.png`);
+                        })
+                )
+                .guard({
+                    params: t.Object({
+                        id: t.Numeric(),
+                    }),
+                    query: t.Object({
+                        p: t.Optional(t.Union([
+                            t.String(),
+                            t.Number()
+                        ]))
+                    })
+                }, (app) => app
+                    .get('/tracks/uploaded-by/:id', async ({ params: { id }, query: { p } }) => {
+                        if (!p) {
+                            p = 0
+                        }
 
-                    return response("", track);
-                })
+                        if (typeof p === 'string') {
+                            p = parseInt(p)
+                        }
+
+                        let tracks = (getTracksByUploaderQuery.all({
+                            $uploader_id: id,
+                            $offset: p*displayTracksPerLoad
+                        }) as track | track[] | null) || []
+
+                        if (!Array.isArray(tracks)) {
+                            tracks = [tracks]
+                        }
+
+                        return response(`Found ${tracks.length}`, tracks)
+                    }))
                 .get('/upload', Bun.file('src/html/upload_example.html'))
                 .guard(
                     {
                         body: t.Object({
                             title: t.String(),
                             artist: t.String(),
-                            cover: t.Union([
-                                t.String(),
-                                t.Number()
-                            ]),
-                            track: t.File()
+                            cover: t.Optional(
+                                t.File({
+                                    type: ["image/jpeg", "image/png"]
+                                })
+                            ),
+                            track: t.File({
+                                type: ["audio/x-flac", "audio/flac", "audio/x-wav", "audio/wav", "audio/mpeg", "audio/ogg", "audio/opus"]
+                            })
                         })
                     },
                     (app) => app.post('/tracks/upload', async ({ body, jwt: { verify }, cookie: { auth } }) => {
-                        if (body.track.type !== "audio/x-flac" && body.track.type !== "audio/mpeg" && body.track.type !== "audio/ogg" && body.track.type !== "audio/opus" && body.track.type !== "audio/wav") {
-                            return error("validation", "body", "/track", "Invalid file format");
-                        }
-
-                        if (body.cover && body.cover.type !== "image/jpeg" && body.cover.type !== "image/png") {
-                            return error("validation", "body", "/cover", "Invalid file format");
-                        }
-
-                        return await saveTrack(body.title, body.artist, body.track, body.cover, db, await verify(auth.value) as user);
+                        return await saveTrack(body.title, body.artist, body.track, body.cover || null, db, await verify(auth.value) as user);
                     })
                 )
         )
